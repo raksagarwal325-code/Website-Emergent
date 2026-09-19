f22f9d724db78bbacef9cb7439714e89b6bfd398
raksagarwal325-code <raks.agarwal325@gmail.com>
2026-09-19 06:36:51 +0530

    Recognize Glass Type as glass cut variant data

--- frontend/src/constants/variantFamilies.js
+++ frontend/src/constants/variantFamilies.js
@@ -1,6 +1,6 @@
 const VARIANT_SPEC_AXES = [
   { key: "glass_colour", label: "Glass colour", specs: ["Glass Colour", "Glass Color", "Colour", "Color"] },
-  { key: "glass_cut", label: "Glass cut / design", specs: ["Glass Cut / Design", "Glass Cut", "Cut Design", "Glass Design", "Glass Pattern"] },
+  { key: "glass_cut", label: "Glass cut / design", specs: ["Glass Cut / Design", "Glass Cut", "Glass Type", "Cut Design", "Glass Design", "Glass Pattern"] },
   { key: "metal_finish", label: "Metal finish", specs: ["Metal Finish", "Finish"] },
   { key: "lights", label: "Lights", specs: ["Number of Lights", "Lights", "Light Count"] },
   { key: "size", label: "Size", specs: ["Dimensions", "Size"] },
